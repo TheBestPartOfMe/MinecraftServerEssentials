f2c7b2eb51432777d1b12353a79e65b29b61c8c5
Maximilian Weber <max@weberwz.de>
2023-12-02 18:18:01 +0100

    Added the backup script

---
+++ backup/autoSave.sh
@@ -0,0 +1,13 @@
+screen -r 8630 -X /save-off              #Insert ur session name for the 8630
+echo   Backup started
+screen -r 8630 -X /save-off
+echo Auto-Save disabled
+TIME=date +%b-%d-%y                      # This Command will read the date.
+FILENAME=backup-server-$TIME.tar.gz      # The filename including the date.
+SRCDIR=UwU                               # Source backup folder.
+DESDIR=backup                            # Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR
+echo Backup saved
+screen -r 8630 -X /save-on
+echo Auto-Save enabled
+echo Done
