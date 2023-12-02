1157a1ee986990bb934ed3f66ff925821ce7e0ec
Max <48136746+TheBestPartOfMe@users.noreply.github.com>
2023-12-02 18:32:11 +0100

    Fixed an Error, which corrupted the time String

--- backup/autoSave.sh
+++ backup/autoSave.sh
@@ -2,7 +2,7 @@ screen -r 8630 -X /save-off              #Insert ur session name for the 8630
 echo   Backup started
 screen -r 8630 -X /save-off
 echo Auto-Save disabled
-TIME=date +%b-%d-%y                      # This Command will read the date.
+TIME=`date +%b-%d-%y`                      # This Command will read the date.
 FILENAME=backup-server-$TIME.tar.gz      # The filename including the date.
 SRCDIR=UwU                               # Source backup folder.
 DESDIR=backup                            # Destination of backup file.
